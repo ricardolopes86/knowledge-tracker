aa0d94f748f1a71aaae5930b55d5f00906d2768b
Ricardo Silva <ricardolopes18@yahoo.com.br>
2017-11-29 23:56:54 +0100

    Created first queries to databse.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://knowledgetracker:p@ssw0rd@localhost/knowledgetracker'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://knowledgetracker:p@ssw0rd@localhost/knowledgetracker'
+SQLALCHEMY_DATABASE_URI = 'mysql://knowledgetracker:p@ssw0rd@localhost/knowledgetracker'
 SECRET_KEY = 'knowledge-tracker'
--- views.py
+++ views.py
@@ -20,7 +20,7 @@ def add_subtopic():
 
 @app.route('/show/progress', methods=['GET'])
 def show_progress():
-    maintopics = db.session.query(MainTopic).all()
+    maintopics = db.session.query(MainTopic).join(Topic).join(SubTopic)
     topics = db.session.query(Topic).all()
     subtopics = db.session.query(SubTopic).all()
     return render_template('show_progress.html', maintopics=maintopics, topics=topics, subtopics=subtopics)
